4745f391f8d880c7073a1c8b4e2672344efb6053
Joseph Lee <joseph.lee22590@gmail.com>
2015-01-08 22:13:24 -0800

    Initial commit - a prototype

---
+++ addon/globalPlugins/easyTableNavigator.py
@@ -0,0 +1,118 @@
+﻿# Easy Table Navigator
+# A global plugin for NvDA
+# Copyright 2015 Joseph Lee, released under GPL
+
+# Allows NVDA to navigate to next or previous column/row just by using arrow keys.
+# The results should be similar to table layer in JAWS.
+
+import globalPluginHandler
+import api
+import config
+import virtualBuffers # For browse mode.
+from NVDAObjects.window import winword # Microsoft Word.
+import textInfos
+import controlTypes
+import addonHandler
+import ui
+import tones #Debugging
+
+# For Microsoft Word: code from MS Word document object, pasted here for convenience.
+def _MSWordTableNavAvailable(document):
+	info=document.makeTextInfo(textInfos.POSITION_CARET)
+	info.expand(textInfos.UNIT_CHARACTER)
+	formatConfig=config.conf['documentFormatting'].copy()
+	formatConfig['reportTables']=True
+	commandList=info.getTextWithFields(formatConfig)
+	if len(commandList)<3 or commandList[1].field.get('role',None)!=controlTypes.ROLE_TABLE or commandList[2].field.get('role',None)!=controlTypes.ROLE_TABLECELL:
+		return False
+	rowCount=commandList[1].field.get('table-rowcount',1)
+	columnCount=commandList[1].field.get('table-columncount',1)
+	rowNumber=commandList[2].field.get('table-rownumber',1)
+	columnNumber=commandList[2].field.get('table-columnnumber',1)
+	try:
+		table=info._rangeObj.tables[1]
+	except COMError:
+		return False
+	return True
+
+def tableNavAvailable(obj=None):
+	# Depending on object type, figure out if we're in a table.
+	focus = api.getFocusObject() if obj is None else obj
+	if isinstance(focus.treeInterceptor, virtualBuffers.VirtualBuffer):
+		try:
+			focus.treeInterceptor._getTableCellCoords(focus.treeInterceptor.selection)
+			return True
+		except LookupError, WindowsError:
+			return False
+	elif isinstance(focus, winword.WordDocument):
+		return _MSWordTableNavAvailable(focus)
+
+class GlobalPlugin(globalPluginHandler.GlobalPlugin):
+
+	# Control table layer entry.
+	tableNav = False
+	tableNavForFocus = False
+
+	def event_gainFocus(self, obj, nextHandler):
+		try:
+			if tableNavAvailable(obj) and self.tableNav:
+				self.bindTableNavGestures()
+			else:
+				self.clearGestureBindings()
+				self.bindGestures(self.__gestures)
+		except WindowsError:
+			pass
+		nextHandler()
+
+	def script_toggleTableNav(self, gesture):
+		if not tableNavAvailable():
+			ui.message("Not in a table")
+		else:
+			if not self.tableNav:
+				self.tableNav = True
+				self.bindTableNavGestures()
+				ui.message("Table navigator on")
+			else:
+				self.tableNav = False
+				self.clearGestureBindings()
+				self.bindGestures(self.__gestures)
+				ui.message("Table navigator off")
+	script_toggleTableNav.__doc__="Toggles table navigation layer on or off. When active, arrow keys can be used to navigate between cells"
+	# Since this plugin really has one command, it is fine to define a script category like this.
+	script_toggleTableNav.category = "Easy Table Navigator"
+
+	# Some utility functions.
+	def bindTableNavGestures(self):
+		self.bindGesture("kb:rightarrow", "nextColumn")
+		self.bindGesture("kb:leftarrow", "prevColumn")
+		self.bindGesture("kb:downarrow", "nextRow")
+		self.bindGesture("kb:uparrow", "prevRow")
+
+	def tableNavHelper(self, obj, script, gesture):
+		obj.treeInterceptor.script(gesture) if isinstance(obj.treeInterceptor, virtualBuffers.VirtualBuffer) else obj.script(gesture)
+
+	# Table navigation commands.
+
+	def script_nextRow(self, gesture):
+		#self.tableNavHelper(api.getFocusObject(), script_nextRow, gesture)
+		focus = api.getFocusObject()
+		focus.treeInterceptor.script_nextRow(gesture) if isinstance(focus.treeInterceptor, virtualBuffers.VirtualBuffer) else focus.script_nextRow(gesture)
+
+	def script_prevRow(self, gesture):
+		#self.tableNavHelper(api.getFocusObject(), script_previousRow, gesture)
+		focus = api.getFocusObject()
+		focus.treeInterceptor.script_previousRow(gesture) if isinstance(focus.treeInterceptor, virtualBuffers.VirtualBuffer) else focus.script_previousRow(gesture)
+
+	def script_nextColumn(self, gesture):
+		#self.tableNavHelper(api.getFocusObject(), script_nextColumn, gesture)
+		focus = api.getFocusObject()
+		focus.treeInterceptor.script_nextColumn(gesture) if isinstance(focus.treeInterceptor, virtualBuffers.VirtualBuffer) else focus.script_nextColumn(gesture)
+
+	def script_prevColumn(self, gesture):
+		#self.tableNavHelper(api.getFocusObject(), script_previousColumn, gesture)
+		focus = api.getFocusObject()
+		focus.treeInterceptor.script_previousColumn(gesture) if isinstance(focus.treeInterceptor, virtualBuffers.VirtualBuffer) else focus.script_previousColumn(gesture)
+
+	__gestures={
+		"kb:NvDA+Shift+T":"toggleTableNav",
+	}
---
+++ readme.md
@@ -0,0 +1,22 @@
+# Easy Table Navigator
+
+*   Author: Joseph Lee
+*   Download [development version][2]
+
+This plugin adds a layer command to use arrow keys to navigate table cells.
+
+Currently supported tables are:
+
+* Browse mode (Internet Explorer, Firefox, etc.).
+* Microsoft Word.
+
+## Commands
+
+* NvDA+Shift+T: Toggles table navigator on and off.
+
+## Changes for 1.0
+
+*   Initial release.
+
+
+[2]: http://addons.nvda-project.org/files/get.php?file=etn-dev
